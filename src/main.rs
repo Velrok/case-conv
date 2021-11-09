9451086e96b2d620d2ce75e1db4108177b318e20
Waldemar (velrok) <34974+Velrok@users.noreply.github.com>
2021-11-09 21:26:29 +0000

    read std in

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buff = String::new();
+    io::stdin().read_line(&mut buff)?;
+    let input = buff.trim();
+    println!("Hello, {}!", input);
+    Ok(())
 }
